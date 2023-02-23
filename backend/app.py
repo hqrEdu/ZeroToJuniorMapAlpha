29123f9959681a9894b301de53679ddfc13fbd95
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-02-23 01:14:17 +0100

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from db_manager import DatabaseManager
+
+app = Flask(__name__)
+
+## post endpoint
+
+
+
+
+## get endpoint
+
+
+
+
+## put endpoint
+
+
+
+## delete enpoint
+
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
