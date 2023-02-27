b50c4cb481df38f5998978db57380ef60bf64793
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-02-27 01:06:33 +0100

    Added code for DELETE endpoint (feature)

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,26 @@
+import os
 from flask import Flask
 from db_manager import DatabaseManager
 
 app = Flask(__name__)
 
+
 ## post endpoint
-
-
 
 
 ## get endpoint
 
 
-
-
 ## put endpoint
 
 
-
 ## delete enpoint
-
-
+@app.route('/delete-user/<discord>')
+def delete_user(discord):
+    db = DatabaseManager(database=os.getenv('database'), user=os.getenv('user'), password=os.getenv('password'),
+                         host=os.getenv('host'))
+    db.delete_user(discord)
+    db.close_connection()
 
 
 if __name__ == '__main__':
