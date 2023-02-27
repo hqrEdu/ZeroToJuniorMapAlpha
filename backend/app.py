184a7ac2b1a7363bfa572e1d9fa977e2ec159ebb
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-02-27 14:59:19 +0100

    Changed the view route, and the way discord_id is sourced from fe - form data vs direct link

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 import os
-
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
 from db_manager import DatabaseManager
 
 app = Flask(__name__)
 
+
+# 400
+#
 
 # POST endpoint
 
@@ -12,7 +14,9 @@ app = Flask(__name__)
 # GET endpoint
 @app.route('/users')
 def get_users():
-    db = DatabaseManager(database=os.getenv('database'), user=os.getenv('user'), password=os.getenv('password'),
+    db = DatabaseManager(database=os.getenv('database'),
+                         user=os.getenv('user'),
+                         password=os.getenv('password'),
                          host=os.getenv('host'))
     all_users = db.get_users()
     db.close_connection()
@@ -23,12 +27,15 @@ def get_users():
 
 
 # DELETE endpoint
-@app.route('/users/<discord>', methods=['DELETE'])
-def delete_user(discord):
-    db = DatabaseManager(database=os.getenv('database'), user=os.getenv('user'), password=os.getenv('password'),
+@app.route('/users', methods=['DELETE'])
+def delete_user():
+    db = DatabaseManager(database=os.getenv('database'),
+                         user=os.getenv('user'),
+                         password=os.getenv('password'),
                          host=os.getenv('host'))
     try:
-        result = db.delete_user(discord)
+        discord_id = request.form.get('discord')
+        result = db.delete_user(discord_id)
         return Response(result, mimetype='application/json'), 200
     except KeyError:
         return jsonify({"error": "User not found or incorrect discord id"})
