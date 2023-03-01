7a6be7519bfa5f82313b25e5c35b8bbc02cb7d85
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-03-01 19:21:33 +0100

    <perf> GET endpoint error handling  Added error handling for http errors. The change will allow for the display of customised error messages to the client

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, abort
 from db_manager import DatabaseManager
 
 app = Flask(__name__)
@@ -12,10 +12,15 @@ app = Flask(__name__)
 # GET endpoint
 @app.route('/users')
 def get_users():
-    db = DatabaseManager(database=os.getenv('database'), user=os.getenv('user'), password=os.getenv('password'),
+    db = DatabaseManager(database=os.getenv('database'),
+                         user=os.getenv('user'),
+                         password=os.getenv('password'),
                          host=os.getenv('host'))
     all_users = db.get_users()
     db.close_connection()
+    if not all_users:
+        abort(500,
+              description='The server has encountered an unexpected error. Please contact the server administrator')
     return Response(all_users, mimetype='application/json'), 200
 
 
@@ -23,7 +28,7 @@ def get_users():
 
 
 # DELETE endpoint
-
+#2
 
 if __name__ == '__main__':
     app.run(debug=True)
