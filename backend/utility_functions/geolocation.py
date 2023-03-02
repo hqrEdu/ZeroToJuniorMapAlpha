d82b3e5613a8a1c4fc58614480af52921bd0502b
andrzej_stopka <andrzej.stopka1996@gmail.com>
2023-03-02 09:50:48 +0100

    perf(BE#06) add else if is no result

--- backend/utility_functions/geolocation.py
+++ backend/utility_functions/geolocation.py
@@ -7,14 +7,15 @@ def postcode_to_city(zip: str):
     postcode = zip
     location = geolocator.geocode(postcode, addressdetails=True)
 
-    city = None
     if location.raw["address"].get("city"):
         city = location.raw["address"]["city"]
     elif location.raw["address"].get("town"):
         city = location.raw["address"]["town"]
     elif location.raw["address"].get("village"):
         city = location.raw["address"]["village"]
-
+    else:
+        city = None
+        
     return city
 
 def get_location_from_city(city):
