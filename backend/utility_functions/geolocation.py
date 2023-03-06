bf61f40b5ef287bb0578886331d845531b3bc7e3
andrzej_stopka <andrzej.stopka1996@gmail.com>
2023-03-06 12:39:43 +0100

    Fix error when zip_code is wrong

--- backend/utility_functions/geolocation.py
+++ backend/utility_functions/geolocation.py
@@ -6,17 +6,16 @@ geolocator = Nominatim(user_agent="z2j_map")
 def postcode_to_city(zip: str):
     postcode = zip
     location = geolocator.geocode(postcode, addressdetails=True)
-
-    if location.raw["address"].get("city"):
-        city = location.raw["address"]["city"]
-    elif location.raw["address"].get("town"):
-        city = location.raw["address"]["town"]
-    elif location.raw["address"].get("village"):
-        city = location.raw["address"]["village"]
+    if location:
+        if location.raw["address"].get("city"):
+            city = location.raw["address"]["city"]
+        elif location.raw["address"].get("town"):
+            city = location.raw["address"]["town"]
+        elif location.raw["address"].get("village"):
+            city = location.raw["address"]["village"]
+        return city
     else:
-        city = None
-        
-    return city
+        return False
 
 def get_location_from_city(city):
     location = geolocator.geocode(city)
