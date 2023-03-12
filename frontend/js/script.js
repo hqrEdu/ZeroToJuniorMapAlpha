f3cb85ad1e016300cdbdd7ea88b9f4ddf7149d71
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2023-03-12 17:45:05 +0100

    styled map section on mobile devices

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,36 +1,34 @@
-const input = document.querySelector('.form__discord-input');
-const cityInput = document.querySelector('.form__city-input');
-const footerYear = document.querySelector('.footer__year');
+const input = document.querySelector('.form__discord-input')
+const cityInput = document.querySelector('.form__city-input')
+const footerYear = document.querySelector('.footer__year')
 const cityText = 'Wpisz swoje miasto...'
-const defaultText = 'Twój nick z Discord';
+const defaultText = 'Twój nick z Discord'
 
 input.addEventListener('focus', function () {
-    if (input.value === defaultText) {
-        input.value = '';
-    }
-});
+	if (input.value === defaultText) {
+		input.value = ''
+	}
+})
 
 input.addEventListener('blur', function () {
-    if (input.value === '') {
-        input.value = defaultText;
-    }
-});
+	if (input.value === '') {
+		input.value = defaultText
+	}
+})
 cityInput.addEventListener('focus', function () {
-    if (cityInput.value === cityText) {
-        cityInput.value = '';
-    }
+	if (cityInput.value === cityText) {
+		cityInput.value = ''
+	}
 })
 
 cityInput.addEventListener('blur', function () {
-    if (cityInput.value === '') {
-        cityInput.value = cityText;
-    }
+	if (cityInput.value === '') {
+		cityInput.value = cityText
+	}
 })
 
 const handleCurrentYear = () => {
-    const year = new Date().getFullYear()
-    footerYear.innerText = year
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
 }
 handleCurrentYear()
-
-
