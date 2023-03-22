57e3f3307f597e978395568e8f46264bc00aba22
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-03-22 12:32:14 +0100

    Adjusted response character encoding, removed redundant empty lines

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,14 @@ from flask import Flask, jsonify
 from user import User
 from utility_functions.errors_converter import convert_to_http_exception
 
-
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 
 @app.errorhandler(Exception)
 def handle_database_error(error):
     error = convert_to_http_exception(error)
     return error
-
-
-# POST endpoint
 
 
 # GET endpoint
@@ -23,13 +20,7 @@ def get_users():
     if not all_users:
         raise Exception
     else:
-        return jsonify(all_users), 200
-
-
-# PUT endpoint
-
-
-# DELETE endpoint
+        return jsonify(all_users, ), 200
 
 
 if __name__ == '__main__':
