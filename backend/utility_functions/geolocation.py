29b2214c8215138dd6bb5f39185d2c0902f8e846
andrzej_stopka <andrzej.stopka1996@gmail.com>
2023-03-22 15:39:12 +0100

    Raise Error if location doesn't exist

--- backend/utility_functions/geolocation.py
+++ backend/utility_functions/geolocation.py
@@ -5,18 +5,19 @@ geolocator = Nominatim(user_agent="z2j_map")
 
 def postcode_to_city(postcode: str):
     location = geolocator.geocode(postcode, addressdetails=True)
-    if location:
-        if location.raw["address"].get("city"):
-            city = location.raw["address"]["city"]
-        elif location.raw["address"].get("town"):
-            city = location.raw["address"]["town"]
-        elif location.raw["address"].get("village"):
-            city = location.raw["address"]["village"]
-        return city
+    if location.raw["address"].get("city"):
+        city = location.raw["address"]["city"]
+    elif location.raw["address"].get("town"):
+        city = location.raw["address"]["town"]
+    elif location.raw["address"].get("village"):
+        city = location.raw["address"]["village"]
     else:
-        return False
+        raise ValueError
+    return city
+
 
 def get_location_from_city(city):
     location = geolocator.geocode(city)
     return location.latitude, location.longitude
+
 
