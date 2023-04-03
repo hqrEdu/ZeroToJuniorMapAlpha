6722096728b9f6faeeb6e7c58f4998b220e38513
Marcin Rogalski <iwannabe.dev@gmail.com>
2023-04-03 21:02:50 +0100

    refactor(#32): apply suggestions from PR review
    Regex checking discord ID has been fixed as per suggestion. Error messages has been changed as proposed and convert_to_http_exception() function from errors_conventer.py file can now accept custom error messages. All data validation moved to request_data_handler.py and split into small functions for reusability.

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from user import User
 from utility_functions.errors_converter import convert_to_http_exception
-import re
+from utility_functions.request_data_handler import verify_POST_request, prepare_data
+
 
 app = Flask(__name__)
+
+
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['JSON_AS_ASCII'] = False
 
@@ -15,32 +18,13 @@ def handle_database_error(error):
     return error
 
 
-def verify_request(data):
-    if 'discord' not in data or 'zip_code' not in data or 'stack' not in data:
-        raise KeyError
-    elif not isinstance(data['discord'], str):
-        print("Discord id must be a string type.")
-        raise TypeError
-    # Check if 'discord' matches the correct pattern:
-    elif not re.findall("[A-Za-z].*#\d\d\d\d", data['discord']):
-        raise TypeError
-    elif request.json['stack'].lower() not in ["be", "fe"]:
-        raise ValueError
-
-
-def prepare_data(data):
-    data['stack'] = data['stack'].lower()
-    return data
-
-
 # POST endpoint
 @app.route('/users', methods=['POST'])
 def add_user():
-    verify_request(request.json)
+    verify_POST_request(request)
     data = prepare_data(request.json)
     user = User()
     result = user.post(data['discord'], data['zip_code'], data['stack'])
-    print(result)
     if result:
         return jsonify(data), 201
 
