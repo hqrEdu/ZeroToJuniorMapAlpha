b1854eb7f58fed9e845cf5d5221516a3d3025ef4
Marcin Rogalski <iwannabe.dev@gmail.com>
2023-04-04 20:36:16 +0100

    feature(#56): Change error messages return type
    Error messages are now being returned as JSON as per request from our frontend team.

--- backend/utility_functions/errors_converter.py
+++ backend/utility_functions/errors_converter.py
@@ -1,18 +1,44 @@
 from werkzeug.exceptions import HTTPException, BadRequest, InternalServerError
 import psycopg2
+from flask import jsonify
+
 
 def convert_to_http_exception(e):
     if isinstance(e, HTTPException):
         return e
     elif isinstance(e, ValueError):
-        return BadRequest(str(e))
+        return jsonify(
+            {
+                "error": "Incorrect value"
+            }
+        ), 400
     elif isinstance(e, KeyError):
-        return BadRequest('Missing required parameter')
+        return jsonify(
+            {
+                "error": "Missing required parameter"
+            }
+        ), 400
     elif isinstance(e, TypeError):
-        return BadRequest('Invalid parameter type')
+        return jsonify(
+            {
+                "error": "Invalid parameter type"
+            }
+        ), 400
     elif isinstance(e, psycopg2.errors.UniqueViolation):
-        return BadRequest('Entered discord username already exists in the database.')
+        return jsonify(
+            {
+                "error": "Entered discord username already exists in the database"
+            }
+        ), 400
     elif isinstance(e, psycopg2.Error):
-        return InternalServerError('Database error')
+        return jsonify(
+            {
+                "error": "Database error"
+            }
+        ), 500
     else:
-        return InternalServerError('Internal Server Error')
+        return jsonify(
+            {
+                "error": "Internal Server Error'"
+            }
+        ), 500
