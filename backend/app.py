cecd6ddf9196323c28fb532e3c1ab7ce2497fab9
Marcin Rogalski <iwannabe.dev@gmail.com>
2023-04-12 20:16:25 +0100

    refactor(#60): apply suggestions from PR review
    Regex checking discord ID has been fixed as per suggestion (#32). All data validation moved to request_data_handler.py and split into small functions for reusability.

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
-import re
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_api.exceptions import APIException
 from user import User
 from utility_functions.api_exceptions import BadRequest
+from utility_functions.request_data_handler import verify_POST_request, prepare_data
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -17,28 +18,10 @@ def handle_database_error(error):
     return response
 
 
-def verify_request(data):
-    if 'discord' not in data or 'zip_code' not in data or 'stack' not in data:
-        raise BadRequest                         # BadRequest(detail="......")
-    elif not isinstance(data['discord'], str):
-        print("Discord id must be a string type.")
-        raise BadRequest
-    # Check if 'discord' matches the correct pattern:
-    elif not re.findall("[A-Za-z].*#\d\d\d\d", data['discord']):
-        raise BadRequest
-    elif request.json['stack'].lower() not in ["be", "fe"]:
-        raise BadRequest
-
-
-def prepare_data(data):
-    data['stack'] = data['stack'].lower()
-    return data
-
-
 # POST endpoint
 @app.route('/users', methods=['POST'])
 def add_user():
-    verify_request(request.json)
+    verify_POST_request(request)
     data = prepare_data(request.json)
     user = User()
     result = user.post(data['discord'], data['zip_code'], data['stack'])
